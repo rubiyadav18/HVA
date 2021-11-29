5e897999fb01ad7cc5ed48b1f542cb9bf531760c
Rubi Yadav <73836805+rubiyadav18@users.noreply.github.com>
2021-11-29 13:55:26 +0530

    Add files via upload

---
+++ pattern_question3.py
@@ -0,0 +1,14 @@
+# 111112
+# 322222
+# 333334
+# 544444
+# 555556
+# n=5
+
+
+n=int(input("enter a ny number---"))
+for i in range(1,n+1):
+    if i%2==0:
+        print(i+1,i,i,i,i,i)
+    else:
+        print(i,i,i,i,i,i+1)
